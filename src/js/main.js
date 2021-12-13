73c13c0ff3d36db4a19aa480ae2073af65a59e8a
clvarley <conorlukevarley@gmail.com>
2021-12-13 00:01:50 +0000

    Very basic JS queue

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const queue = window.cmdQueue || [];
+const header = require("./src/header.js");
+
+queue.push( header );
+
+// Execute the queue if items are pending
+document.addEventListener( "DOMContentLoaded", () => {
+    if ( queue.length ) {
+        queue.forEach( (callback) => callback() );
+    }
+
+    window.cmdQueue = { push: (callback) => callback() };
+});
